e0e92b3ddd34f550c88b2ff1a4cdab1869c0a902
Justin Toman <justintoman@gmail.com>
2022-09-29 20:03:48 -0500

    Delete port and package lock (pnpm lock instead)

--- app/env.ts
+++ app/env.ts
@@ -3,7 +3,6 @@ export const ENV = cleanEnv(process.env, {
   NEO4J_USER: str(),
   NEO4J_URI: str(),
   NEO4J_PASSWORD: str(),
-  PORT: port(),
   TMDB_API_KEY: str(),
   TMDB_API_URL: str(),
   BING_API_KEY: str(),
